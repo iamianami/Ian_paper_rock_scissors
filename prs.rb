5750ebf07af0f749502e18c8f1cabd803427a56b
iamianami <ian751029@gmail.com>
2015-01-01 19:55:43 +0800

    put the tie rule in

--- prs.rb
+++ prs.rb
@@ -20,18 +20,21 @@ end
 
 loop do 
 
-begin
-puts "Choice one (P/R/S):"
-player_choice = gets.chomp.downcase
-end until CHOICES.keys.include?(player_choice)
+  begin
+    puts "Choice one (P/R/S):"
+    player_choice = gets.chomp.downcase
+  end until CHOICES.keys.include?(player_choice)
 
-computer_choice = CHOICES.keys.sample
+    computer_choice = CHOICES.keys.sample
 
- if player_choice == "p" && computer_choice == "r" || 
-    player_choice == "r" && computer_choice == "s" ||
-    player_choice == "s" && computer_choice == "p"
-    display_winning_message(player_choice)
-    puts "You Won"
+ if    player_choice == computer_choice
+       puts "It's tie"
+       
+ elsif player_choice == "p" && computer_choice == "r" || 
+       player_choice == "r" && computer_choice == "s" ||
+       player_choice == "s" && computer_choice == "p"
+       display_winning_message(player_choice)
+       puts "You Won"
  else
     display_winning_message(computer_choice)
     puts "Computer Won!"
